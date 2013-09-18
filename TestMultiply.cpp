9b118c944675aa41f5d39834de137beb589caf8f
Jack Morrison <jackmorrison1@gmail.com>
2013-09-18 08:14:17 -0400

    Adds more to example test.

--- TestMultiply.cpp
+++ TestMultiply.cpp
@@ -2,10 +2,25 @@
 
 int multiply(int a, int b)
 {
-    return a*b;
+    int accum = 0;
+    for (int i = 0; i < b; ++i) {
+        accum += a;
+    }
+
+    return accum;
 }
 
 TEST(TestMultiply, Zero)
 {
+    EXPECT_EQ(0, multiply(0, 0));
+}
+
+TEST(TestMultiply, Nonzero)
+{
     EXPECT_EQ(12, multiply(3, 4));
+}
+
+TEST(TestMultiply, OneNegative)
+{
+    EXPECT_EQ(-12, multiply(-3, 4));
 }
